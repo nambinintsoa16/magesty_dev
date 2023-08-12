8bfca6f1182a0330cc0beba0a3d9801dac71b260
nambinintsoa16 <nambinintsoa16@gmail.com>
2023-08-12 13:06:34 +0300

    modification htaccess

--- application/config/database.php
+++ application/config/database.php
@@ -72,12 +72,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-//_1ytsgm
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '185.98.131.149',
-	'username' => 'combo1588192',
-	'password' => 'gtDT45trlk1758',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
 	'database' => 'combo1588192',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
