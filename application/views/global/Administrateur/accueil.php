7399913452f74e7f85b86404c42103b6b14cca83
nambinintsoa16 <nambinintsoa16@gmail.com>
2023-10-19 22:30:40 +0200

    update accueil admin -> update liste enquette

--- application/views/global/Administrateur/accueil.php
+++ application/views/global/Administrateur/accueil.php
@@ -7,12 +7,12 @@
                     <button  class="btn btn-sm p-1" style="margin-top:-15px;width:50px;background:#04B4AE;height:40px;"><i class="fa fa-money fa-2x text-white"></i></button>
                 </div>
                 <div class="col-md-12 text-right mb-2">
-                    <strong class="font-weight-bold count"><?=$transaction?><br><span>Génére liste enquête</span></strong>
+                    <strong class="font-weight-bold count">&nbsp;<br><span>Génére liste enquête</span></strong>
                 </div>
                 <a href="#" id="update_liste_enquette" class=" card-footer text-white nav-link p-1 text-center"  style="background: #04B4AE;">Clicker ici<i class="fa fa-arrow-circle-right ml-2"></i></a>
             </div>
         </div>
-        <div class="col-sm-6 col-md-2">
+        <div class="col-sm-6 col-md-3">
             <div class=" card card-stats card-round ">
                 <div class="col-md-2">
                     <button  class="btn btn-sm text-center" style="margin-top:-15px;width:50px;height:40px;background:#088A4B;"><i class="fa fa-cubes fa-2x text-white"></i></button>
@@ -23,7 +23,7 @@
                 <a href="operatrice/Produits/Liste_des_produits" class="card-footer text-white acclink nav-link p-1 text-center" style="background: #088A4B;">Plus d'info <i class="fa fa-arrow-circle-right ml-2"></i></a>
             </div>
         </div>
-        <div class="col-sm-6 col-md-2">
+        <div class="col-sm-6 col-md-3">
             <div class=" card card-stats card-round ">
                 <div class="col-md-2">
                     <button  class="btn btn-warning btn-sm p-1" style="margin-top:-15px;width:50px;height:40px;"><i class="fa fa-users fa-2x text-white"></i></button>
@@ -34,18 +34,7 @@
                 <a href="<?=base_url('operatrice/Clients/Mes_clients')?>" class="card-footer bg-warning acclink text-white nav-link p-1 text-center">Plus d'info <i class="fa fa-arrow-circle-right ml-2"></i></a>
             </div>
         </div>
-        <div class="col-sm-6 col-md-2">
-            <div class=" card card-stats card-round ">
-                <div class="col-md-2">
-                    <button  class="btn btn-sm" style="margin-top:-15px;width:50px;height:40px;background: #86B404;"><i class="fa fa-pencil-square-o fa-2x text-white"></i></button>
-                </div>
-                <div class="col-md-12 text-right mb-2">
-                    <strong class="font-weight-bold count"><br><span>Tâche</span></strong>
-                </div>
-                <a href="<?=base_url('operatrice/Tache')?>" class="card-footer text-white nav-link p-1 text-center" style="background: #86B404;">Plus d'info <i class="fa fa-arrow-circle-right ml-2"></i></a>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-2">
+        <div class="col-sm-6 col-md-3">
             <div class=" card card-stats card-round ">
                 <div class="col-md-2">
                     <button  class="btn btn-danger btn-sm" style="margin-top:-15px;width:50px;height:40px;"><i class="fa fa-calendar fa-2x text-white"></i></button>
--- assets/js/administrateur.js
+++ assets/js/administrateur.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
 	$('#update_liste_enquette').on('click',function(event){
 		event.preventDefault();
+		loding();
 		$.post(base_url+'Administrateur/update_liste_enquette',function(data){
-			console.log(data);
+			stopload();
 		});
 	});
+
+	function stopload() {
+    $('.jconfirm-open').remove();
+	}
+	   function loding() {
+        var htmls = '<style>.jconfirm-content{opacity:1;}.jconfirm .jconfirm-box{height: 125px !important; text-align:center;}.jconfirm .jconfirm-box div.jconfirm-closeIcon {display:none !important;}.spinner {margin: 10px auto;width: 50px;height: 40px;text-align: center;font-size: 10px;}.spinner > div {background-color: #31ce36;height: 100%;width: 6px;display: inline-block;-webkit-animation: sk-stretchdelay 1.2s infinite ease-in-out;animation: sk-stretchdelay 1.2s infinite ease-in-out;}.spinner .rect2 {-webkit-animation-delay: -1.1s;animation-delay: -1.1s;}.spinner .rect3 {-webkit-animation-delay: -1.0s;animation-delay: -1.0s;}.spinner .rect4 {-webkit-animation-delay: -0.9s;animation-delay: -0.9s;}.spinner .rect5 {-webkit-animation-delay: -0.8s;animation-delay: -0.8s;}@-webkit-keyframes sk-stretchdelay {0%, 40%, 100% { -webkit-transform: scaleY(0.4) }  20% { -webkit-transform: scaleY(1.0) }}@keyframes sk-stretchdelay {0%, 40%, 100% { transform: scaleY(0.4);-webkit-transform: scaleY(0.4);}  20% { transform: scaleY(1.0);-webkit-transform: scaleY(1.0);}}</style><div class="text-center" style="font-size:14px;"><span class="text-center">Traitement en cours ...</span></div><div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>';
+        $.dialog({
+            "title": "",
+            "content": htmls,
+            "show": true,
+            "modal": true,
+            "close": false,
+            "closeOnMaskClick": false,
+            "closeOnEscape": false,
+            "dynamic": false,
+            "height": 150,
+            "fixedDimensions": true
+        });
+    }
 
 });
