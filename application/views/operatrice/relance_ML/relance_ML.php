11c43830b18f7ce65b08bd6f2e3c69a83a5bd5c9
jodevdago <jogasy.rabefialy@gmail.com>
2023-11-18 08:00:37 +0300

    fic margin

--- application/views/operatrice/relance_ML/relance_ML.php
+++ application/views/operatrice/relance_ML/relance_ML.php
@@ -4,7 +4,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="infoModalLabel">Information sur l'observation</h5>
+                    <h5 class="modal-title ml-4" id="infoModalLabel">Les informations sur l'observation</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
